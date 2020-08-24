54d34144958c669b7ecee9338c92fcc483b7ca78
EricGrantland <66648537+EricGrantland@users.noreply.github.com>
2020-08-24 13:30:21 -0400

    Update Main.java

--- src/com/myApps/Main.java
+++ src/com/myApps/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        BankAccount account = new BankAccount("My Account", "12345", 500.00);
+        BankAccount account = new BankAccount("My Account", 0.00);
         printOptions();
 
         int choice = 0;

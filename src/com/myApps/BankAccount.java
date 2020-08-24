65b1c53e14d8d2e38d05efebbab5fd4e8e035e05
ericgrantland <ericgrantland24@gmail.com>
2020-08-24 13:27:51 -0400

    Initial Commit: TransactionTracker app

---
+++ src/com/myApps/BankAccount.java
@@ -0,0 +1,84 @@
+package com.myApps;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+
+public class BankAccount {
+    private String accountName;
+    private String accountNumber;
+    private double balance;
+    private double startingBalance;
+    private List<String> transactions;
+
+    public BankAccount(String accountName, String accountNumber, double balance) {
+        this.accountName = accountName;
+        this.accountNumber = accountNumber;
+        if(balance > 0) {
+            this.balance = balance;
+        } else {
+            this.balance = 0;
+        }
+        this.startingBalance = this.balance;
+        this.transactions = new ArrayList<String>();
+        transactions.add("Starting Balance: " + startingBalance);
+    }
+
+    public void showBalance() {
+        System.out.println("Account Balance: " + this.balance);
+    }
+
+    public boolean depositFunds(double depositAmount) {
+            if (depositAmount > 0) {
+                this.balance += depositAmount;
+                System.out.println("$" + depositAmount + " has been deposited into " + accountName + ".\nBalance is now: " + this.balance);
+                transactions.add("+$" + depositAmount + " deposit made.\n\tTotal Balance: $" + this.balance);
+                return true;
+            }
+            System.out.println("Can only deposit a positive amount");
+            return false;
+    }
+
+    public boolean withdrawFunds(double withdrawalAmount) {
+            if (withdrawalAmount > 0) {
+                this.balance -= withdrawalAmount;
+                System.out.println("$" + withdrawalAmount + " has been withdrawn from " + accountName + ".\nBalance is now: " + this.balance);
+                transactions.add("-$" + withdrawalAmount + " withdrawal made.\n\tTotal Balance: $" + this.balance);
+                return true;
+            }
+
+            System.out.println("Can only withdraw a positive amount");
+            return false;
+
+
+    }
+
+    public void listTransactions() {
+        System.out.println("Transaction History:");
+        for(String transaction : transactions) {
+            System.out.println("\t" + transaction);
+        }
+        System.out.println("Current Balance: " + this.balance);
+    }
+
+    public void showLastTransaction() {
+        if(transactions.size() > 1) {
+            System.out.println("Last transaction:\n\t" + transactions.get(transactions.size() - 1));
+        } else {
+            System.out.println("No transactions have been made since the initial deposit. \nAccount Balance: " + this.balance);
+        }
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+}
---
+++ src/com/myApps/Main.java
@@ -0,0 +1,78 @@
+package com.myApps;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BankAccount account = new BankAccount("My Account", "12345", 500.00);
+        printOptions();
+
+        int choice = 0;
+        boolean appOpen = true;
+
+        while (appOpen) {
+            Scanner scanner = new Scanner(System.in);
+            try {
+                choice = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Enter valid number");
+                printOptions();
+                continue;
+            }
+
+            switch (choice) {
+                case 1:
+                    account.showBalance();
+                    break;
+                case 2:
+                    try {
+                        System.out.println("\tEnter deposit amount:");
+                        double depositAmount = scanner.nextDouble();
+                     account.depositFunds(depositAmount);
+                    } catch (InputMismatchException e) {
+                        System.out.println("Invalid input entered");
+                        printOptions();
+                    }
+                    break;
+                case 3:
+                    try {
+                        System.out.println("\tEnter withdrawal amount:");
+                        double withdrawalAmount = scanner.nextDouble();
+                        account.withdrawFunds(withdrawalAmount);
+                    } catch (InputMismatchException e) {
+                        System.out.println("Invalid input entered");
+                        printOptions();
+                    }
+                    break;
+                case 4:
+                    account.showLastTransaction();
+                    break;
+                case 5:
+                    account.listTransactions();
+                    break;
+                case 6:
+                    System.out.println("Closing App");
+                    appOpen = false;
+                    break;
+                default:
+                    System.out.println("Please enter a valid number between 1 - 6:\n");
+                    printOptions();
+                    break;
+            }
+        }
+
+    }
+
+    public static void printOptions() {
+        System.out.println("\nSelect an option from the menu below:");
+        System.out.println("\t1. Show account balance" +
+                "\n\t2. Make deposit" +
+                "\n\t3. Withdraw funds" +
+                "\n\t4. Show last transaction" +
+                "\n\t5. Show full transaction history" +
+                "\n\t6. Quit Application");
+    }
+}
